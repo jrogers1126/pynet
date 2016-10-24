f4dd93253d0154cc76b99142604bc49bdc12196a
Josh Rogers <silverfox541@gmail.com>
2016-10-23 22:43:52 -0500

    fix import filname

--- class2/excercise2.py
+++ class2/excercise2.py
@@ -1,11 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env pythoTELNET_PORTn
 ##Import stuff
-import pprint from pprint
-import pynet-lib,telnetlib, pysnmp
+import pynetlib, telnetlib
+
+#Define stuff
+TELNET_PORT=23
+TELNET_TIMEOUT=1
 
 def main():
     #Write program here
-
+    telnetlib.Telnet('184.105.247.70', TELNET_PORT, TELNET_TIMEOUT)
+    remote_conn.read_until(<string_pattern>, TELNET_TIMEOUT)
+    remote_conn.read_very_eager()
+    remote_conn.write('show ip int brief' + '\n')
+    remote_conn.close() 
 
 ##Only run if not called by another file/program
 if __name__ == "__main__":
