ba5bc27e6fabca0b586ea5567224e8f531dfd608
Josh Rogers <silverfox541@gmail.com>
2016-10-23 21:08:16 -0500

    add class2

---
+++ functions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+__author__ = "Josh Rogers"
+__credits__ = ["Josh Rogers"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Josh Rogers"
+__email__ = "2016@tybox.net"
+__status__ = "Prototype"
+
+##Import stuff
+import pprint from pprint
+import telnetlib, pysnmp
+
+def hr(s, c='#'):
+    #not worth figuring the term width cross-platform, assume 80
+    width = 80
+    padding = width - 2 - len(s)
+    print c.center(padding/2,c) + ' ' + s + ' ' + c.center(padding/2,c)
+
+def main():
+    #Write program here
+
+##Only run if not called by another file/program
+if __name__ == "__main__":
+    main()
+
---
+++ template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+__author__ = "Josh Rogers"
+__credits__ = ["Josh Rogers"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Josh Rogers"
+__email__ = "2016@tybox.net"
+__status__ = "Prototype"
+
+##Import stuff
+import pprint from pprint
+import telnetlib, pysnmp
+
+def main():
+    #Write program here
+
+
+##Only run if not called by another file/program
+if __name__ == "__main__":
+    main()
+
+
