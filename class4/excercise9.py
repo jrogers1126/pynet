ee12cc38b931f439d674902a9695dc03b69ac6ab
Josh Rogers <2016@tybox.net>
2016-11-06 17:56:44 -0800

    ok, got multiprocess working, however the output all comes out simultaneously.  This is a problem, need some sort of 'message queue' to hold on to the output, and process the output one at a time.

--- class4/excercise9.py
+++ class4/excercise9.py
@@ -5,7 +5,8 @@ from netmiko import ConnectHandler
 #pynet.py is found in root git directory
 import pynet, multiprocessing
 
-def showCommand(hostname, net_connect, cmd):
+def showCommand(hostname, cmd):
+    net_connect = ConnectHandler(**eval('pynet.' + hostname))
     print net_connect.find_prompt() + cmd
     print net_connect.send_command(cmd)
     return
@@ -14,17 +15,14 @@ def showCommand(hostname, net_connect, cmd):
 if __name__ == "__main__":
     ios_arp = "show ip arp"
     junos_arp = "show arp no-resolve"
-    rtr1 = ConnectHandler(**pynet.rtr1)
-    rtr2 = ConnectHandler(**pynet.rtr2)
-    srx1 = ConnectHandler(**pynet.srx1)
     devices = [
-        { 'hostname': 'rtr1', 'net_connect': rtr1, 'cmd': ios_arp },
-        { 'hostname': 'rtr2', 'net_connect': rtr2, 'cmd': ios_arp },
-        { 'hostname': 'srx1', 'net_connect': srx1, 'cmd': junos_arp },
+        { 'hostname': 'rtr1', 'cmd': ios_arp },
+        { 'hostname': 'rtr2', 'cmd': ios_arp },
+        { 'hostname': 'srx1', 'cmd': junos_arp },
     ]
     jobs = []
     for device in devices:
-        p = multiprocessing.Process(target=showCommand, args=(device['hostname'],device['net_connect'],device['cmd'],))
+        p = multiprocessing.Process(target=showCommand, args=(device['hostname'],device['cmd'],))
         jobs.append(p)
         p.start()
 
